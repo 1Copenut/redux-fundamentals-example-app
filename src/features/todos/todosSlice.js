6a6bf754867c3cea5a935619b23bf6c3eafcff49
Trevor Pierce <934879+1Copenut@users.noreply.github.com>
2025-01-06 16:26:51 -0600

    Step 4, first commit of reducer, slices.

--- src/features/todos/todosSlice.js
+++ src/features/todos/todosSlice.js
@@ -1,8 +1,10 @@
-const initialState = [
-  { id: 0, text: 'Learn React', completed: true },
-  { id: 1, text: 'Learn Redux', completed: false, color: 'purple' },
-  { id: 2, text: 'Build something fun!', completed: false, color: 'blue' },
-]
+// const initialState = [
+//   { id: 0, text: 'Learn React', completed: true },
+//   { id: 1, text: 'Learn Redux', completed: false, color: 'purple' },
+//   { id: 2, text: 'Build something fun!', completed: false, color: 'blue' },
+// ]
+
+const initialState = []
 
 function nextTodoId(todos) {
   const maxId = todos.reduce((maxId, todo) => Math.max(todo.id, maxId), -1)
@@ -15,7 +17,7 @@ export default function todosReducer(state = initialState, action) {
       return [
         ...state,
         {
-          id: nextTodoId(state.todos),
+          id: nextTodoId(state),
           text: action.payload,
           completed: false,
         },
